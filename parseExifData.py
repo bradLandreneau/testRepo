2624ca3bbf095ce9ce873fc79d8e01de69282a4c
bradLandreneau <bradlandreneau@gmail.com>
2016-12-12 15:01:17 -0600

    Create parseExifData.py

---
+++ parseExifData.py
@@ -0,0 +1,66 @@
+from subprocess import PIPE, run
+import sys
+import json
+
+#pass in path to file you wish to parse exif data from
+pathToFile = sys.argv[1];
+#print(pathToFile)
+
+
+def getExifData(video):
+
+    # run the command "exiftool.exe -j yourInputVideo"
+    command = ["exiftool.exe", "-j", video]
+
+    # we want the ouput of the command, So PIPE stdout/stderr
+    result = run(command, stdout=PIPE, stderr=PIPE, universal_newlines=True)
+
+    # for python to treat this as a json, remove the brackets from the start and finish
+    # there may be a better way to do this, but this seems to work fine.
+    outputString = str(result.stdout).strip().replace("[", "").replace("]", "")
+
+    # test output of exiftool command
+    # print(outputString)
+
+    # load the string as a json object, then search for the make key
+    jsonDict = json.loads(outputString)
+
+    videoInfo = {}
+
+    #building personalized dictionary for exif data (key/values that are important)
+    if "FileName" in jsonDict:
+        videoInfo["Filename"] = jsonDict["FileName"]
+    else:
+        videoInfo["Filname"] = "NULL"
+
+    if "Make" in jsonDict:
+        videoInfo["Make"] = jsonDict["Make"]
+    else:
+        videoInfo["Make"] = "NULL"
+
+    if "Duration" in jsonDict:
+        videoInfo["Duration"] = jsonDict["Duration"]
+    else:
+        videoInfo["Duration"] = "NULL"
+
+    if "DateTimeOriginal" in jsonDict:
+        videoInfo["DateTimeOriginal"] = jsonDict["DateTimeOriginal"]
+    else:
+        videoInfo["DateTimeOriginal"] = "DateTimeOriginal"
+
+    return videoInfo
+
+videoExifData = getExifData(sys.argv[1])
+
+for key, value in videoExifData.items():
+    print(key, ":", value)
+
+
+
+
+
+
+
+
+
+
